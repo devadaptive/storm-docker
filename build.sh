c1bc789f3e995b1c2c363cab084803f68b0183f7
Paul Callahan <paul.callahan@gmail.com>
2016-01-21 18:45:40 -0800

    allow build.sh to be run from anywhere

--- build.sh
+++ build.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
+STORM_SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 echo  '########################################'
 echo  '## building storm base                ##'
 echo  '########################################'
-docker build -t="devadaptive/storm:0.10.0" storm
+docker build -t="devadaptive/storm:0.10.0" ${STORM_SCRIPT_DIR}/storm
 #docker tag devadaptive/storm:0.10.0 devadaptive/storm:latest
 
 echo  '########################################'
 echo  '## building storm-nimbus              ##'
 echo  '########################################'
-docker build -t="devadaptive/storm-nimbus:0.10.0" storm-nimbus
+docker build -t="devadaptive/storm-nimbus:0.10.0" ${STORM_SCRIPT_DIR}/storm-nimbus
 #docker tag devadaptive/storm-nimbus:0.10.0 devadaptive/storm-nimbus:latest
 
 echo  '########################################'
 echo  '## building storm-supervisor          ##'
 echo  '########################################'
-docker build -t="devadaptive/storm-supervisor:0.10.0" storm-supervisor
+docker build -t="devadaptive/storm-supervisor:0.10.0" ${STORM_SCRIPT_DIR}/storm-supervisor
 #docker tag devadaptive/storm-supervisor:0.10.0 devadaptive/storm-supervisor:latest
 
 echo  '########################################'
 echo  '## building storm-ui                  ##'
 echo  '########################################'
-docker build -t="devadaptive/storm-ui:0.10.0" storm-ui
+docker build -t="devadaptive/storm-ui:0.10.0" ${STORM_SCRIPT_DIR}/storm-ui
 #docker tag devadaptive/storm-ui:0.10.0 devadaptive/storm-ui:latest
