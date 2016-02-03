9e63ea6c1f67e379bd1c5edd2dad78927f464fb5
Paul Callahan <paul.callahan@gmail.com>
2016-02-03 01:03:00 -0800

    revamp docker storm images:  fix host name setting

--- storm/start-supervisor.sh
+++ storm/start-supervisor.sh
@@ -1,6 +1,21 @@
-sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
-sed -i -e "s/%nimbus%/$NIMBUS_PORT_6627_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
+#!/usr/bin/env bash
 
-echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
+: ${HOSTNAME:=`hostname`}
+: ${ZOOKEEPER_HOST:=zk}
+
+
+if [ ! -f $STORM_HOME/conf/storm.yaml ]; then
+    mv $STORM_HOME/conf/storm-template.yaml $STORM_HOME/conf/storm.yaml
+    sed -i -e "s/%zookeeper%/${ZOOKEEPER_HOST}/g" $STORM_HOME/conf/storm.yaml
+    sed -i -e "s/%nimbus%/${HOSTNAME}/g" $STORM_HOME/conf/storm.yaml
+    # IS_NIMBUS_SERVER is set in the Dockerfile
+    if [ "$IS_NIMBUS_SERVER" = true ] ; then
+        echo "storm.local.hostname: \"${HOSTNAME}\"" >> $STORM_HOME/conf/storm.yaml
+    fi
+fi
+
+echo "********** using config **********"
+cat $STORM_HOME/conf/storm.yaml
+echo "**********************************"
 
 /usr/sbin/sshd && supervisord
