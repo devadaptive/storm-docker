826ea7db5f885213c7c3be60ee02621dedf6c406
Paul Callahan <paul.callahan@gmail.com>
2016-02-03 16:31:42 -0800

    revamp docker storm images:  fix host name setting

--- storm/start-supervisor.sh
+++ storm/start-supervisor.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 
-: ${HOSTNAME:=`hostname`}
 : ${ZOOKEEPER_HOST:=zk}
+: ${NIMBUS_HOST:=nimbus}
 
+
+IP=`hostname -i | awk '{print $1}'`
 
 if [ ! -f $STORM_HOME/conf/storm.yaml ]; then
     mv $STORM_HOME/conf/storm-template.yaml $STORM_HOME/conf/storm.yaml
-    sed -i -e "s/%zookeeper%/${ZOOKEEPER_HOST}/g" $STORM_HOME/conf/storm.yaml
-    sed -i -e "s/%nimbus%/${HOSTNAME}/g" $STORM_HOME/conf/storm.yaml
     # IS_NIMBUS_SERVER is set in the Dockerfile
     if [ "$IS_NIMBUS_SERVER" = true ] ; then
-        echo "storm.local.hostname: \"${HOSTNAME}\"" >> $STORM_HOME/conf/storm.yaml
+        sed -i -e "s/%nimbus%/${IP}/g" $STORM_HOME/conf/storm.yaml
+    else
+        sed -i -e "s/%nimbus%/${NIMBUS_HOST}/g" $STORM_HOME/conf/storm.yaml
     fi
+    sed -i -e "s/%zookeeper%/${ZOOKEEPER_HOST}/g" $STORM_HOME/conf/storm.yaml
+    echo "storm.local.hostname: \"${IP}\"" >> $STORM_HOME/conf/storm.yaml
 fi
 
-echo "********** using config **********"
+echo "************ using config ************"
 cat $STORM_HOME/conf/storm.yaml
-echo "**********************************"
+echo "**************************************"
 
 /usr/sbin/sshd && supervisord
