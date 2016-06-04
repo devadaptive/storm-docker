59a0dd0c24a92323fd45d02cd11226f86483225b
Paul Callahan <paul.callahan@gmail.com>
2016-06-03 18:33:14 -0700

    upgrade to storm 1.0.1

--- build.sh
+++ build.sh
@@ -4,23 +4,23 @@ STORM_SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo  '########################################'
 echo  '## building storm base                ##'
 echo  '########################################'
-docker build -t="devadaptive/storm:0.10.0" ${STORM_SCRIPT_DIR}/storm
-#docker tag devadaptive/storm:0.10.0 devadaptive/storm:latest
+docker build -t="devadaptive/storm:1.0.1" ${STORM_SCRIPT_DIR}/storm
+#docker tag devadaptive/storm:1.0.1 devadaptive/storm:latest
 
 echo  '########################################'
 echo  '## building storm-nimbus              ##'
 echo  '########################################'
-docker build -t="devadaptive/storm-nimbus:0.10.0" ${STORM_SCRIPT_DIR}/storm-nimbus
-#docker tag devadaptive/storm-nimbus:0.10.0 devadaptive/storm-nimbus:latest
+docker build -t="devadaptive/storm-nimbus:1.0.1" ${STORM_SCRIPT_DIR}/storm-nimbus
+#docker tag devadaptive/storm-nimbus:1.0.1 devadaptive/storm-nimbus:latest
 
 echo  '########################################'
 echo  '## building storm-supervisor          ##'
 echo  '########################################'
-docker build -t="devadaptive/storm-supervisor:0.10.0" ${STORM_SCRIPT_DIR}/storm-supervisor
-#docker tag devadaptive/storm-supervisor:0.10.0 devadaptive/storm-supervisor:latest
+docker build -t="devadaptive/storm-supervisor:1.0.1" ${STORM_SCRIPT_DIR}/storm-supervisor
+#docker tag devadaptive/storm-supervisor:1.0.1 devadaptive/storm-supervisor:latest
 
 echo  '########################################'
 echo  '## building storm-ui                  ##'
 echo  '########################################'
-docker build -t="devadaptive/storm-ui:0.10.0" ${STORM_SCRIPT_DIR}/storm-ui
-#docker tag devadaptive/storm-ui:0.10.0 devadaptive/storm-ui:latest
+docker build -t="devadaptive/storm-ui:1.0.1" ${STORM_SCRIPT_DIR}/storm-ui
+#docker tag devadaptive/storm-ui:1.0.1 devadaptive/storm-ui:latest
