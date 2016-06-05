575fde7d287f57657170c405df84437595b52464
Paul Callahan <paul.callahan@gmail.com>
2016-06-04 21:05:08 -0700

    upgrade to storm 1.0.1

--- build.sh
+++ build.sh
@@ -4,23 +4,23 @@ STORM_SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo  '########################################'
 echo  '## building storm base                ##'
 echo  '########################################'
-docker build -t="devadaptive/storm:1.0.1" ${STORM_SCRIPT_DIR}/storm
+docker build --no-cache -t="devadaptive/storm:1.0.1" ${STORM_SCRIPT_DIR}/storm
 #docker tag devadaptive/storm:1.0.1 devadaptive/storm:latest
 
 echo  '########################################'
 echo  '## building storm-nimbus              ##'
 echo  '########################################'
-docker build -t="devadaptive/storm-nimbus:1.0.1" ${STORM_SCRIPT_DIR}/storm-nimbus
+docker build --no-cache -t="devadaptive/storm-nimbus:1.0.1" ${STORM_SCRIPT_DIR}/storm-nimbus
 #docker tag devadaptive/storm-nimbus:1.0.1 devadaptive/storm-nimbus:latest
 
 echo  '########################################'
 echo  '## building storm-supervisor          ##'
 echo  '########################################'
-docker build -t="devadaptive/storm-supervisor:1.0.1" ${STORM_SCRIPT_DIR}/storm-supervisor
+docker build --no-cache -t="devadaptive/storm-supervisor:1.0.1" ${STORM_SCRIPT_DIR}/storm-supervisor
 #docker tag devadaptive/storm-supervisor:1.0.1 devadaptive/storm-supervisor:latest
 
 echo  '########################################'
 echo  '## building storm-ui                  ##'
 echo  '########################################'
-docker build -t="devadaptive/storm-ui:1.0.1" ${STORM_SCRIPT_DIR}/storm-ui
+docker build --no-cache -t="devadaptive/storm-ui:1.0.1" ${STORM_SCRIPT_DIR}/storm-ui
 #docker tag devadaptive/storm-ui:1.0.1 devadaptive/storm-ui:latest
