afeae3b6746ea19c79ec29b16bba6bf261f3300e
Paul Callahan <paul.callahan@gmail.com>
2016-06-13 20:08:45 -0700

    no cache

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ STORM_SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo  '########################################'
 echo  '## building storm base                ##'
 echo  '########################################'
-docker build -t="devadaptive/storm:0.10.0" ${STORM_SCRIPT_DIR}/storm
+docker build --no-cache -t="devadaptive/storm:0.10.0" ${STORM_SCRIPT_DIR}/storm
 #docker tag devadaptive/storm:0.10.0 devadaptive/storm:latest
 
 echo  '########################################'
 echo  '## building storm-nimbus              ##'
 echo  '########################################'
-docker build -t="devadaptive/storm-nimbus:0.10.0" ${STORM_SCRIPT_DIR}/storm-nimbus
+docker build --no-cache -t="devadaptive/storm-nimbus:0.10.0" ${STORM_SCRIPT_DIR}/storm-nimbus
 #docker tag devadaptive/storm-nimbus:0.10.0 devadaptive/storm-nimbus:latest
 
 echo  '########################################'
@@ -22,5 +22,5 @@ docker build -t="devadaptive/storm-supervisor:0.10.0" ${STORM_SCRIPT_DIR}/storm-
 echo  '########################################'
 echo  '## building storm-ui                  ##'
 echo  '########################################'
-docker build -t="devadaptive/storm-ui:0.10.0" ${STORM_SCRIPT_DIR}/storm-ui
+docker build --no-cache -t="devadaptive/storm-ui:0.10.0" ${STORM_SCRIPT_DIR}/storm-ui
 #docker tag devadaptive/storm-ui:0.10.0 devadaptive/storm-ui:latest
